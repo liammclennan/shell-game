c20a3414f9240e75592c49108538a1f22c0e9cc4
Liam McLennan <liam.mclennan@gmail.com>
2011-10-11 17:33:45 +1000

    moved to a common base class for shuffleable objects

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-var Board, Cup, templates;
-Cup = (function() {
-  function Cup(container, position) {
+var Board, Cup, Pea, ShuffleObject, templates;
+var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+  for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+  function ctor() { this.constructor = child; }
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor;
+  child.__super__ = parent.prototype;
+  return child;
+};
+ShuffleObject = (function() {
+  function ShuffleObject(container, position) {
     this.container = container;
     this.position = position;
     this._render();
   }
-  Cup.prototype._render = function() {
-    $(templates.cup_markup).appendTo($('body'));
-    return null;
+  ShuffleObject.prototype._render = function() {
+    return $(this._markup()).css('left', this.container[this.position]).appendTo($('body'));
+  };
+  return ShuffleObject;
+})();
+Cup = (function() {
+  __extends(Cup, ShuffleObject);
+  function Cup() {
+    Cup.__super__.constructor.apply(this, arguments);
+  }
+  Cup.prototype._markup = function() {
+    return templates.cup_markup;
   };
   return Cup;
 })();
 Board = (function() {
-  function Board() {}
+  function Board() {
+    this.left = 50;
+    this.centre = $('body').width() / 2 - 200;
+    this.right = $('body').width() - 450;
+    console.log("" + this.left + " " + this.centre + " " + this.right);
+  }
   return Board;
 })();
+Pea = (function() {
+  __extends(Pea, ShuffleObject);
+  function Pea() {
+    Pea.__super__.constructor.apply(this, arguments);
+  }
+  Pea.prototype._markup = function() {
+    return templates.pea_markup;
+  };
+  return Pea;
+})();
 templates = {
-  cup_markup: '<img src="images/cup.png" alt="cup" />'
+  cup_markup: '<img src="images/cup.png" alt="cup" class="cup" />'
 };
 $(function() {
-  var one;
-  return one = new Cup(new Board(), 'left');
+  var board, one, three, two;
+  board = new Board();
+  one = new Cup(board, 'left');
+  two = new Cup(board, 'centre');
+  return three = new Cup(board, 'right');
 });
--- src/app.coffee
+++ src/app.coffee
@@ -1,19 +1,32 @@
-
-class Cup
+class ShuffleObject
   constructor: (@container, @position) ->
     @_render()
     
   _render: ->
-    $(templates.cup_markup).appendTo($('body'));
-    null
+    $(@_markup()).css('left', @container[@position]).appendTo($('body'));
+
+class Cup extends ShuffleObject
+  _markup: ->
+      templates.cup_markup
   
 class Board
-  
-  
-templates =
-  cup_markup: '<img src="images/cup.png" alt="cup" />'
+  constructor: ->
+    @left = 50
+    @centre = $('body').width() / 2 - 200
+    @right = $('body').width() -  450
+    console.log "#{@left} #{@centre} #{@right}"
 
-# comment
+class Pea extends ShuffleObject
+    _markup: ->
+      templates.pea_markup
+    
+templates =
+  cup_markup: '<img src="images/cup.png" alt="cup" class="cup" />'
+
+
 
 $ ->
-  one = new Cup(new Board(), 'left')
+  board = new Board()
+  one = new Cup(board, 'left')
+  two = new Cup(board, 'centre')
+  three = new Cup(board, 'right')
