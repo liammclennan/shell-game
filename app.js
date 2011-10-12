1c740f38a3be22cf52dfcf8defcf4a69f947f533
Liam McLennan <liam.mclennan@gmail.com>
2011-10-12 18:19:14 +1000

    can play the game now

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var Board, Cup, Pea, ShuffleObject, templates;
-var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+var Board, Cup, Operator, Pea, ShuffleObject, templates;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; }, __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
   for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
   function ctor() { this.constructor = child; }
   ctor.prototype = parent.prototype;
@@ -13,16 +13,46 @@ ShuffleObject = (function() {
     this.position = position;
     this._render();
   }
+  ShuffleObject.prototype.move = function(position, offset) {
+    return this._animate(this.el, position, offset);
+  };
+  ShuffleObject.prototype._animate = function(element, position, offset) {
+    element.css('top', offset);
+    return element.animate({
+      left: this.container[position]
+    }, this.container.speed, __bind(function() {
+      element.css('top', this.container.vertical_baseline());
+      return this.position = position;
+    }, this));
+  };
   ShuffleObject.prototype._render = function() {
-    return $(this._markup()).css('left', this.container[this.position]).appendTo($('body'));
+    return this.el = $(this._markup()).css('left', this.container[this.position]).appendTo($('body'));
   };
   return ShuffleObject;
 })();
 Cup = (function() {
   __extends(Cup, ShuffleObject);
-  function Cup() {
-    Cup.__super__.constructor.apply(this, arguments);
+  function Cup(container, position, operator) {
+    this.container = container;
+    this.position = position;
+    this.operator = operator;
+    Cup.__super__.constructor.call(this, this.container, this.position);
+    this.el.click(__bind(function() {
+      return this.operator.reveal();
+    }, this));
   }
+  Cup.prototype.hide = function(pea) {
+    this.pea = pea;
+  };
+  Cup.prototype.move = function(position, offset) {
+    if (this.pea != null) {
+      this._move_pea(position, offset);
+    }
+    return Cup.__super__.move.call(this, position, offset);
+  };
+  Cup.prototype._move_pea = function(position, offset) {
+    return this._animate(this.pea.el, position, offset);
+  };
   Cup.prototype._markup = function() {
     return templates.cup_markup;
   };
@@ -33,8 +63,16 @@ Board = (function() {
     this.left = 50;
     this.centre = $('body').width() / 2 - 200;
     this.right = $('body').width() - 450;
-    console.log("" + this.left + " " + this.centre + " " + this.right);
   }
+  Board.prototype.vertical_baseline = function() {
+    return 100;
+  };
+  Board.prototype.high = function() {
+    return this.vertical_baseline() + 50;
+  };
+  Board.prototype.low = function() {
+    return this.vertical_baseline() - 50;
+  };
   return Board;
 })();
 Pea = (function() {
@@ -42,6 +80,12 @@ Pea = (function() {
   function Pea() {
     Pea.__super__.constructor.apply(this, arguments);
   }
+  Pea.prototype.forward = function() {
+    return this.el.css('z-index', 2);
+  };
+  Pea.prototype.backward = function() {
+    return this.el.css('z-index', 0);
+  };
   Pea.prototype._markup = function() {
     return templates.pea_markup;
   };
@@ -51,11 +95,63 @@ templates = {
   cup_markup: '<img src="images/cup.png" alt="cup" class="cup" />',
   pea_markup: '<img src="images/ball.png" alt="pea" class="pea" />'
 };
+Operator = (function() {
+  function Operator(speed) {
+    this.speed = speed;
+    this.board = new Board();
+    this.left = new Cup(this.board, 'left', this);
+    this.centre = new Cup(this.board, 'centre', this);
+    this.right = new Cup(this.board, 'right', this);
+    this.pea = new Pea(this.board, 'right');
+    this.right.hide(this.pea);
+    setTimeout(__bind(function() {
+      return this.pea.el.css('z-index', 0);
+    }, this), 500);
+  }
+  Operator.prototype.shuffle = function(times) {
+    var count, _results;
+    this._reset();
+    _results = [];
+    for (count = 1; count <= 5; count++) {
+      _results.push(setTimeout(__bind(function() {
+        return this._swap();
+      }, this), (this.speed * count) + 10));
+    }
+    return _results;
+  };
+  Operator.prototype.reveal = function() {
+    return this.pea.forward();
+  };
+  Operator.prototype._reset = function() {
+    return this.pea.backward();
+  };
+  Operator.prototype._swap = function() {
+    var from, left_hand, right_hand, to, _ref;
+    left_hand = right_hand = this._select_cup();
+    while (left_hand === right_hand) {
+      right_hand = this._select_cup();
+    }
+    _ref = [left_hand.position, right_hand.position], from = _ref[0], to = _ref[1];
+    left_hand.move(to, this.board.high());
+    return right_hand.move(from, this.board.low());
+  };
+  Operator.prototype._select_cup = function() {
+    var val;
+    val = Math.random();
+    if (val < 1 / 3) {
+      return this.left;
+    }
+    if ((1 / 3 <= val && val < 2 / 3)) {
+      return this.centre;
+    }
+    return this.right;
+  };
+  return Operator;
+})();
 $(function() {
-  var board, one, pea, three, two;
-  board = new Board();
-  one = new Cup(board, 'left');
-  two = new Cup(board, 'centre');
-  three = new Cup(board, 'right');
-  return pea = new Pea(board, 'right');
+  var o;
+  o = new Operator(500);
+  return $('#shuffle').click(function() {
+    return o.shuffle();
+  });
 });
--- src/app.coffee
+++ src/app.coffee
@@ -1,34 +1,109 @@
 class ShuffleObject
   constructor: (@container, @position) ->
     @_render()
+  
+  move: (position, offset)->
+    @_animate @el, position, offset
+  
+  _animate: (element, position, offset) ->
+    element.css('top', offset)
+    element.animate({
+      left: @container[position]
+    }, @container.speed, =>
+      element.css('top', @container.vertical_baseline())
+      @position = position
+    )
     
   _render: ->
-    $(@_markup()).css('left', @container[@position]).appendTo($('body'));
+    @el = $(@_markup()).css('left', @container[@position]).appendTo($('body'));
 
 class Cup extends ShuffleObject
-  _markup: ->
-      templates.cup_markup
+  constructor: (@container, @position, @operator) ->
+    super @container, @position
+    @el.click(=>
+      @operator.reveal()
+    )
   
+  hide: (@pea) ->
+  
+  move: (position, offset) ->
+    @_move_pea(position, offset) if @pea?
+    super(position, offset)
+  
+  _move_pea: (position, offset) ->
+    @_animate @pea.el, position, offset
+  
+  _markup: ->
+    templates.cup_markup
+      
 class Board
   constructor: ->
     @left = 50
     @centre = $('body').width() / 2 - 200
     @right = $('body').width() -  450
-    console.log "#{@left} #{@centre} #{@right}"
+  
+  vertical_baseline: -> 100
+  
+  high: -> @vertical_baseline() + 50
+  
+  low: -> @vertical_baseline() - 50
 
 class Pea extends ShuffleObject
-    _markup: ->
-      templates.pea_markup
+  forward: ->
+    @el.css 'z-index', 2
+
+  backward: ->
+    @el.css 'z-index', 0
+
+  _markup: ->
+    templates.pea_markup
     
 templates =
   cup_markup: '<img src="images/cup.png" alt="cup" class="cup" />',
   pea_markup: '<img src="images/ball.png" alt="pea" class="pea" />'
 
+class Operator
+  constructor: (@speed)->
+    @board = new Board()
+    @left = new Cup(@board, 'left', @)
+    @centre = new Cup(@board, 'centre', @)
+    @right = new Cup(@board, 'right', @)
+    @pea = new Pea(@board, 'right')
+    @right.hide @pea
+    setTimeout(=>
+      @pea.el.css('z-index',0)
+    , 500)
+    
+  shuffle: (times) ->
+    @_reset()
+    for count in [1..5]
+      setTimeout(=>
+        @_swap()
+      , (@speed * count) + 10)
+  
+  reveal: ->
+    @pea.forward()
+    # display result    
+  
+  _reset: ->
+    @pea.backward()
+    # hide result
+    
+  _swap: ->
+    left_hand = right_hand = @_select_cup()
+    right_hand = @_select_cup() while left_hand == right_hand
+    [from,to] = [left_hand.position, right_hand.position]
+    left_hand.move(to, @board.high())
+    right_hand.move(from, @board.low())
 
+  _select_cup: ->
+    val = Math.random()
+    if val < 1/3 then return @left
+    if 1/3 <= val < 2/3 then return @centre
+    return @right
 
 $ ->
-  board = new Board()
-  one = new Cup(board, 'left')
-  two = new Cup(board, 'centre')
-  three = new Cup(board, 'right')
-  pea = new Pea(board, 'right')
+  o = new Operator(500)
+  $('#shuffle').click(-> o.shuffle())
+  
+  
